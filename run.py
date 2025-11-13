93074b11a08dd875daf76603fa4e88ccf45a7df5
jaydeep gajera <jaydeepgajera@jaydeeps-MacBook-Pro.local>
2025-11-13 15:56:19 +0530

    migration works

--- run.py
+++ run.py
@@ -7,10 +7,14 @@ app = create_app()
 
 if __name__ == "__main__":
 
+    reload_dirs = ["app"] if os.getenv("ENV") == "development" else None
+
     uvicorn.run(
         "run:app",
         host="0.0.0.0",
         port=int(os.getenv("PORT", "8801")),
         reload=os.getenv("ENV", "development") == "development",
+        reload_dirs=reload_dirs,
+        reload_excludes=["alembic/versions/*"],
         log_level=os.getenv("LOG_LEVEL", "info").lower(),
     ) 
