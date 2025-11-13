df54d3af0563f71511ea8eb3ff1912d3cded7a50
jaydeep gajera <jaydeepgajera@jaydeeps-MacBook-Pro.local>
2025-11-13 17:33:25 +0530

    v-1.1

--- docs/remove-files/temp-remove.sh
+++ docs/remove-files/temp-remove.sh
@@ -1,1 +1,13 @@
-i want to remove list of files like .env .end.dev readme.md app/api/v1/router.py app/config/constant.py alembic/versions (folder) 
+#!/bin/bash
+
+# This script removes specified files and directories.
+
+# Remove individual files
+rm -f .env
+rm -f .env.dev
+rm -f README.md
+rm -f app/api/v1/router.py
+rm -f app/config/constants.py
+
+# Remove directory
+rm -rf alembic/versions
